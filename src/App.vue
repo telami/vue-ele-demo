b685c67d72934e26d2a16a07e9e5b8ae86178334
telami <zhuhongchao@i-counting.cn>
2018-11-27 20:54:03 +0800

    [feat] ban zoom and add common fonts and add eslint rules

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <cube-button> qq</cube-button>
+    <Button>youtube</Button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Button from 'cube-ui/src/components/button/button'
 
 export default {
   name: 'app',
   components: {
-    Button,
-    HelloWorld
+    Button
   }
 }
 </script>
 <style lang="stylus">
-#app
-  font-family 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+
 </style>
