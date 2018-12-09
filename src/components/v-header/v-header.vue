0ac7d39c930b41ec19fb41e9ab7b15f93a34c44b
telami <907848106@qq.com>
2018-12-09 22:48:46 +0800

    [style] header style

--- src/components/v-header/v-header.vue
+++ src/components/v-header/v-header.vue
@@ -44,49 +44,51 @@
 <style scoped lang="stylus" ref="stylesheet/stylus">
   @import "../../common/stylus/mixin.styl"
   .header
-    position relative
-    overflow hidden
+    position: relative
+    overflow: hidden
     color: #fff
-    background-color: black
+    background: rgba(7, 17, 27, 0.5)
     .content-wrapper
-      padding 24px 12px 18px 24px
+      position: relative
+      padding: 24px 12px 18px 24px
+      font-size: 0
       .avatar
         display: inline-block
-        vertical-align top
+        vertical-align: top
         img
-          border-radius 2px
+          border-radius: 2px
       .content
         display: inline-block
         margin-left: 16px
-        font-size 14px
         .title
           margin: 2px 0 8px 0
           .brand
             display: inline-block
-            vertical-align top
+            vertical-align: top
             width: 30px
-            height 18px
+            height: 18px
             bg-image('brand')
-            background-size 30px 18px
-            background-repeat no-repeat
+            background-size: 30px 18px
+            background-repeat: no-repeat
           .name
-            margin-left 6px
+            margin-left: 6px
             font-size: 16px
             line-height: 18px
             font-weight: bold
+
         .description
-          margin-bottom 10px
+          margin-bottom: 10px
           line-height: 12px
-          font-size 12px
+          font-size: 12px
         .support
           .icon
             display: inline-block
-            vertical-align top
+            vertical-align: top
             width: 12px
             height: 12px
-            margin-right 4px
-            background-size 12px 12px
-            background-repeat no-repeat
+            margin-right: 4px
+            background-size: 12px 12px
+            background-repeat: no-repeat
             &.decrease
               bg-image('decrease_1')
             &.discount
@@ -98,6 +100,6 @@
             &.special
               bg-image('special_1')
           .text
-            line-height:12px
-            font-size 10px
+            line-height: 12px
+            font-size: 10px
 </style>
