a45e4a2ffdfee647a28ca42a70ad9476f44cb5c0
telami <907848106@qq.com>
2018-12-09 22:47:13 +0800

    [init] header

--- src/components/v-header/v-header.vue
+++ src/components/v-header/v-header.vue
@@ -13,7 +13,7 @@
           {{seller.description}}/{{seller.deliveryTime}}分钟送达
         </div>
         <div v-if="seller.supports" class="support">
-          <span class="icon"></span>
+          <span class="icon" :class="iconCls[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
@@ -32,10 +32,72 @@
           return {}
         }
       }
+    },
+    computed: {
+      iconCls() {
+        return ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+      }
     }
   }
 </script>
 
 <style scoped lang="stylus" ref="stylesheet/stylus">
-
+  @import "../../common/stylus/mixin.styl"
+  .header
+    position relative
+    overflow hidden
+    color: #fff
+    background-color: black
+    .content-wrapper
+      padding 24px 12px 18px 24px
+      .avatar
+        display: inline-block
+        vertical-align top
+        img
+          border-radius 2px
+      .content
+        display: inline-block
+        margin-left: 16px
+        font-size 14px
+        .title
+          margin: 2px 0 8px 0
+          .brand
+            display: inline-block
+            vertical-align top
+            width: 30px
+            height 18px
+            bg-image('brand')
+            background-size 30px 18px
+            background-repeat no-repeat
+          .name
+            margin-left 6px
+            font-size: 16px
+            line-height: 18px
+            font-weight: bold
+        .description
+          margin-bottom 10px
+          line-height: 12px
+          font-size 12px
+        .support
+          .icon
+            display: inline-block
+            vertical-align top
+            width: 12px
+            height: 12px
+            margin-right 4px
+            background-size 12px 12px
+            background-repeat no-repeat
+            &.decrease
+              bg-image('decrease_1')
+            &.discount
+              bg-image('discount_1')
+            &.guarantee
+              bg-image('guarantee_1')
+            &.invoice
+              bg-image('invoice_1')
+            &.special
+              bg-image('special_1')
+          .text
+            line-height:12px
+            font-size 10px
 </style>
