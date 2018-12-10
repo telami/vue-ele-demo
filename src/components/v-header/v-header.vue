b5b4e32237ff7689853858b410c95c816496ab1d
telami <907848106@qq.com>
2018-12-10 18:16:13 +0800

    [feat] star component

--- src/components/v-header/v-header.vue
+++ src/components/v-header/v-header.vue
@@ -17,12 +17,12 @@
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
       </div>
-      <div v-if="seller.supports" class="support-count">
+      <div @click="showDetail" v-if="seller.supports" class="support-count">
         <span class="count">{{seller.supports.length}}个</span>
         <i class="icon-keyboard_arrow_right"></i>
       </div>
     </div>
-    <div class="bulletin-wrapper">
+    <div class="bulletin-wrapper" @click="showDetail">
       <span class="bulletin-title"></span>
       <span class="bulletin-text">{{seller.bulletin}}</span>
       <i class="icon-keyboard_arrow_right"></i>
@@ -30,12 +30,25 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%"/>
     </div>
+    <div v-show="detailShow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <h1 class="name">{{seller.name}}</h1>
+          <Star :score="seller.score" :size="36"></Star>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import Star from "components/star/star";
   export default {
     name: "v-header",
+    components: {Star},
     props: {
       seller: {
         type: Object,
@@ -44,9 +57,19 @@
         }
       }
     },
+    data() {
+      return {
+        detailShow: false
+      }
+    },
     computed: {
       iconCls() {
         return ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+      }
+    },
+    methods: {
+      showDetail() {
+        this.detailShow = true
       }
     }
   }
@@ -167,4 +190,32 @@
       height: 100%
       z-index -1
       filter blur(10px)
+    .detail
+      position: fixed
+      z-index: 100
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      overflow: auto
+      background rgba(7, 17, 27, 0.8)
+      .detail-wrapper
+        min-height: 100%
+        width: 100%;
+        .detail-main
+          margin-top: 64px
+          padding-bottom: 64px
+          .name
+            line-height: 16px
+            text-align: center
+            font-size: 16px
+            font-weight: 700
+
+      .detail-close
+        position: relative;
+        width: 30px
+        height: 30px
+        margin: -64px auto 0 auto
+        clear: both
+        font-size: 32px
 </style>
