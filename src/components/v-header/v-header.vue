d6904839341b8712092dcf6366e4818897915dc9
telami <907848106@qq.com>
2018-12-10 22:58:11 +0800

    [feat] flex

--- src/components/v-header/v-header.vue
+++ src/components/v-header/v-header.vue
@@ -34,7 +34,14 @@
       <div class="detail-wrapper clearfix">
         <div class="detail-main">
           <h1 class="name">{{seller.name}}</h1>
-          <Star :score="seller.score" :size="36"></Star>
+          <div class="star-wrapper">
+            <star :score="seller.score" :size="48"></star>
+          </div>
+          <div class="title">
+            <div class="line"></div>
+            <div class="text">用户信息</div>
+            <div class="line"></div>
+          </div>
         </div>
       </div>
       <div class="detail-close">
@@ -45,10 +52,11 @@
 </template>
 
 <script>
-  import Star from "components/star/star";
+  import Star from 'components/star/star'
+
   export default {
-    name: "v-header",
-    components: {Star},
+    name: 'v-header',
+    components: { Star },
     props: {
       seller: {
         type: Object,
@@ -210,7 +218,23 @@
             text-align: center
             font-size: 16px
             font-weight: 700
-
+          .star-wrapper
+            margin-top 18px
+            padding: 2px 0
+            text-align center
+          .title
+            display flex
+            width 80%
+            margin 30px auto 24px auto
+            .line
+              flex: 1;
+              position relative
+              top: -6px
+              border-bottom 1px solid rgba(255, 255, 255, 0.2)
+            .text
+              padding: 0 12px
+              font-size:14px
+              font-weight:bold
       .detail-close
         position: relative;
         width: 30px
